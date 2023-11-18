2bf53d08e031ffb9ee505bc0f6f0523615d461dd
zoey-on-github <88806187+zoey-on-github@users.noreply.github.com>
2023-11-18 03:09:20 -0800

    jfioew

--- src/main/java/io/zoeyongithub/CustomItem.java
+++ src/main/java/io/zoeyongithub/CustomItem.java
@@ -1,17 +1,23 @@
 package io.zoeyongithub;
 
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
+import org.apache.commons.io.input.TeeInputStream;
 
 public class CustomItem extends Item {
     public CustomItem(Settings settings) {
         super(settings);
     }
-
+@Override
+public void appendTooltip (ItemStack itemStack, World world, List tooltip, TooltipContext tooltipContext){
+        tooltip.add(Text.translatable("item.juliecrraft.custom_item.tooltip"));
+}
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
         playerEntity.setHealth(0);
